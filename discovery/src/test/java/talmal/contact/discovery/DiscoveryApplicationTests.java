ade47c84e23bf8c30513389bd7031a6964c2a679
TalMal-ContactMe <mailrelay999@gmail.com>
2022-12-15 15:20:17 +0200

    initial commit

---
+++ discovery/src/test/java/talmal/contact/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,12 @@
+package talmal.contact.discovery;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
